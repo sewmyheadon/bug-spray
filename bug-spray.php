448a1dcac77064783f782f3e93e5080a1d94d8f4
Eric Amundson <eric@ivycat.com>
2018-12-21 10:36:18 -0800

    update version, date

--- bug-spray.php
+++ bug-spray.php
@@ -3,17 +3,17 @@
  *
  * @package     IvyCat
  * @author      Eric Amundson <eric@ivycat.com>
- * @copyright   2017 IvyCat, Inc.
+ * @copyright   2018 IvyCat, Inc.
  * @license     GPL-2.0+
  *
  * @wordpress-plugin
  * Plugin Name: Bug Spray
  * Plugin URI:  https://ivycat.com/wordpress-plugins/
  * Description: Easily use Kint and Whoops to provide more helpful and attractive PHP errors and debug info. Requires Composer.
- * Version:     1.0.2
+ * Version:     1.0.3
  * Author:      IvyCat
  * Author URI:  https://ivycat.com
- * Text Domain: ivycat-plugin-name
+ * Text Domain: ivycat-bug-spray
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
